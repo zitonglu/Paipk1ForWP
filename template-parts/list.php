8931a79756a045c4bed5489f479999072f29cce1
zitonglu <910109610@qq.com>
2016-11-27 19:55:00 +0800

    change list

--- template-parts/list.php
+++ template-parts/list.php
@@ -9,10 +9,11 @@
 ?>
 <li class="media" id="post-<?php the_ID(); ?>">
 	<h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h4>
+<?php if (has_post_thumbnail()): ?>
 	<div class="media-left">
 		<div class="media-box">
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php paipk1_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>"></a>
-			<div class="tim"><a href="<?php the_permalink(); ?>"><?php the_time('M') ?><br>><?php the_time('d') ?></a>
+			<div class="tim"><a href="<?php the_permalink(); ?>"><?php the_time('M') ?><br><?php the_time('d') ?></a>
 			</div>
 			<div class="cat">
 				<?php the_category(',') ?>
@@ -20,12 +21,15 @@
 		</div>
 	</div>
 	<div class="media-body">
+<?php else: ?>
+	<div>
+<?php endif; ?>
 		<h6>
 		<i class="glyphicon glyphicon-time"></i>&nbsp;<?php the_time('Y-m-d') ?>&nbsp;
 		<?php if(has_tag()): ?><i class="glyphicon glyphicon-tags"></i>&nbsp;
 			<?php the_tags('',',') ?>
 		<?php endif ?>
-		<i class="glyphicon glyphicon-eye-open"></i>&nbsp;<a href="#"><?php post_views(); ?></a></a>
+		<i class="glyphicon glyphicon-eye-open"></i>&nbsp;<a href="#"><?php post_views(); ?></a>
 		</h6>
 		<?php the_excerpt() ?>
 		<p class="clearfix"></p>
